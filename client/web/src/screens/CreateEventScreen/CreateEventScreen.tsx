846abe2077645f4e9469324feec93ce25e1a99e3
Alexandra Burnside <burnsideally@gmail.com>
2021-02-02 19:26:46 +0000

    feat(create event screen): renders form with inputs

--- client/web/src/screens/CreateEventScreen/CreateEventScreen.tsx
+++ client/web/src/screens/CreateEventScreen/CreateEventScreen.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React from "react";
+import LocationAutoCompleteInput from "../../components/LocationAutoCompleteInput/LocationAutoCompleteInput";
+import "./CreateEventScreen.css";
 
-function CreateEventScreen() {
-  return <div></div>;
+const handleImageUpload = () => {
+  const image = document.createElement("img");
+  image.src = URL.createObjectURL(file);
+  // <div className="selectedPhotoOutput">{photo}</div>
+};
+
+const mockArrayTags = ["Food", "Adventure", "Nature"];
+
+export function CreateEventScreen() {
+  return (
+    <div className="container">
+      <div className="AddEvent">
+        <h2> Create Event</h2>
+        <form>
+          <h4>Upload Photo</h4>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            multiple={false}
+          />
+          <h4>Event Name</h4>
+          <input
+            type="text"
+            name="EventName"
+            placeholder="Type Event Name..."
+          />
+          <h4>Location</h4>
+
+          <LocationAutoCompleteInput placeholder="Location" />
+          <label>Tags:</label>
+          <input type="text" name="Tags" placeholder="Input Tags" />
+          {mockArrayTags.map((tag) => (
+            <div>{tag}</div>
+          ))}
+
+          <h2>From:</h2>
+          <input type="datetime-local" name="EventDate" />
+          <h2>To:</h2>
+          <input type="datetime-local" name="EventDate" />
+          <div>
+            <input
+              type="checkbox"
+              id="event"
+              name="isEventPrivate"
+              value="private"
+            />
+            <label htmlFor="event">Private Event</label>
+          </div>
+          <div>
+            <label>Description</label>
+          </div>
+          <div>
+            <textarea name="description" cols={40} rows={5} />
+          </div>
+          <div className="rating">
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+          </div>
+          <button type="button">Share</button>
+          <h3>Budget Level</h3>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-export default CreateEventScreen;
--- client/web/src/screens/CreateTipScreen/CreateTipScreen.tsx
+++ client/web/src/screens/CreateTipScreen/CreateTipScreen.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+// import React from "react";
 
-function CreateTipScreen() {
-  return <div></div>;
+// export function CreateTipScreen() {
+//   return (
+//     <div>
+//       <h1>hey</h1>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import LocationAutoCompleteInput from "../../components/LocationAutoCompleteInput/LocationAutoCompleteInput";
+import "./CreateTipScreen.css";
+
+const handleImageUpload = () => {
+  console.log("Image Change");
+};
+
+const mockArrayTags = ["Food", "Adventure", "Nature"];
+
+export function CreateTipScreen() {
+  return (
+    <div className="container">
+      <div className="AddTip">
+        <h2>Create Travel Tip</h2>
+        <form>
+          <div className="photoUploadContainer">
+            <label>Upload Photo</label>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              multiple={false}
+            />
+          </div>
+          <div className="titleInputContainer">
+            <label>Tip Name</label>
+            <input type="text" name="TipName" placeholder="Type Tip Name..." />
+          </div>
+          <div className="locationInputContainer">
+            <label>Location</label>
+            <LocationAutoCompleteInput placeholder="Location" />
+          </div>
+          <div className="tagSelectionContainer">
+            <label>Tags:</label>
+            <input type="text" name="Tags" placeholder="Input Tags" />
+            {mockArrayTags.map((tag) => (
+              <div className="tagOutput">{tag}</div>
+            ))}
+          </div>
+          <div className="descriptionInputContainer">
+            <label>Description</label>
+            <textarea name="description" cols={40} rows={5} />
+          </div>
+          <div className="ratingInputContainer">
+            <div className="rating">
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+            </div>
+          </div>
+          <div className="priceLevelInputContainer">
+            <div className="rating">
+              <span>$</span>
+              <span>$</span>
+              <span>$</span>
+            </div>
+          </div>
+          <button type="button">Share</button>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-export default CreateTipScreen;
