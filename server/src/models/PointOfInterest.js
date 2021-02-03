e46e052ab317da2a657abe55d7ddbf36bc7db44a
Hough-Lab <hough@outlook.com>
2021-02-03 14:57:58 +0000

    models fixed again

--- server/src/models/PointOfInterest.js
+++ server/src/models/PointOfInterest.js
@@ -11,14 +11,10 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   PointOfInterest.associate = (models) => {
-    // PointOfInterest.belongsToMany(models.User, { through: 'PointOfInterest_FavoritedBy' })
-    // PointOfInterest.belongsToMany(models.Review, { through: 'PointOfInterest_Reviews' })
-    // PointOfInterest.belongsToMany(models.Event, { through: 'PointOfInterest_Events', constraints: false })
+    PointOfInterest.belongsToMany(models.User, { through: 'PointOfInterest_FavoritedBy' })
+    PointOfInterest.belongsToMany(models.Review, { through: 'PointOfInterest_Reviews' })
+    PointOfInterest.belongsToMany(models.Event, { through: 'PointOfInterest_Events' })
     PointOfInterest.hasOne(models.Location); //? location of the POI
-    PointOfInterest.hasMany(models.Event); //? list of events at the POI
-    PointOfInterest.hasMany(models.Review); //? list of revies for the POI
-    PointOfInterest.hasMany(models.ReviewTag); //? POI tags extracted from the review tags for this poi
-    PointOfInterest.hasMany(models.User); //? users who favorite this POI
   };
   return PointOfInterest;
 };
